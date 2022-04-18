c02452e9a83d4c3de698f3703774e7a91309266d
Martin Burolla <mburolla@xpanxion.com>
2022-04-17 20:01:44 -0400

    update

---
+++ src/main/java/com/xpanxion/assignments/groupd/student17.java
@@ -0,0 +1,19 @@
+package com.xpanxion.assignments.groupd;
+
+public class student17 {
+    public static void main(String[] args) {
+        ex1();
+    }
+
+    private static void ex1() {
+        System.out.println("Ex. 1");
+    }
+
+    private static void ex2() {
+        System.out.println("Ex. 2");
+    }
+
+    private static void ex3() {
+        System.out.println("Ex. 3");
+    }
+}
