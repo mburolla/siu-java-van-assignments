6fedf3af0fd480e35c05b20c4aa7a7b18f853984
Martin Burolla <mburolla@xpanxion.com>
2022-04-24 14:46:08 -0400

    update

--- src/main/java/com/xpanxion/assignments/groupa/student1.java
+++ src/main/java/com/xpanxion/assignments/groupa/student1.java
@@ -11,34 +11,14 @@ public class student1 {
     }
 
     private static void ex1() {
-
-
-        int[] ints = {1, 2, 3};
-        var intList = new ArrayList<Integer>(ints.length);
-        for (int i : ints)
-        {
-            intList.add(i);
-        }
-
-
-        System.out.println(addNumbers(intList));
-
-        //System.out.println("Ex. 1");
+        System.out.println("Ex. 1");
     }
-
-    private static Integer addNumbers(ArrayList<Integer> numList) {
-        // TODO: Implement this function.
-        return 0;
-    }
-
 
     private static void ex2() {
-
         System.out.println("Ex. 2");
     }
 
     private static void ex3() {
-
         System.out.println("Ex. 3");
     }
 }
